38dad16548cfcd390f968ffc0d972b287ac05151
희은 <datakubit2@gmail.com>
2025-09-09 01:23:07 +0900

    Feat: 프로젝트 설정

---
+++ src/main/java/com/rabbittick/persister/MarketDataPersisterApplication.java
@@ -0,0 +1,13 @@
+package com.rabbittick.persister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketDataPersisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketDataPersisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rabbittick/persister/MarketDataPersisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbittick.persister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDataPersisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
