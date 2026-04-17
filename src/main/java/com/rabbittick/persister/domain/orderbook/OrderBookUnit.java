06902437d1b773f7997580fba59617b98bf355a7
Heeeun Cho <heeeun.spring@gmail.com>
2026-04-18 02:31:50 +0900

    Perf: Orderbook_unit 시퀀스 allocationSize 500으로 조정

--- src/main/java/com/rabbittick/persister/domain/orderbook/OrderBookUnit.java
+++ src/main/java/com/rabbittick/persister/domain/orderbook/OrderBookUnit.java
@@ -36,7 +36,7 @@ public class OrderBookUnit {
 	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "orderbook_unit_seq")
-	@SequenceGenerator(name = "orderbook_unit_seq", sequenceName = "orderbook_unit_seq", allocationSize = 50)
+	@SequenceGenerator(name = "orderbook_unit_seq", sequenceName = "orderbook_unit_seq", allocationSize = 500)
 	private Long id;
 
 	/**
